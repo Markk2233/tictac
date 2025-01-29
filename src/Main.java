96151f662e5b47d28ca9bf0082cb7ffe50748c36
Mark <saxarov07@bk.ru>
2025-01-29 21:50:30 +0400

    completely finished tictactoe

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+
 import java.util.Scanner;
 
 class TicTacToe {
@@ -20,7 +21,7 @@ class TicTacToe {
         System.out.println("hodi eblan");
         int col = sc.nextInt();
         int row = sc.nextInt();
-        while (row < 1 || row > 3 || col < 1 || col > 3) {
+        while (row < 1 || row > 3 || col < 1 || col > 3 || board[row-1][col-1] != '-') {
             System.out.println("loh, vvodi zanovo");
             row = sc.nextInt();
             col = sc.nextInt();
@@ -29,7 +30,7 @@ class TicTacToe {
     }
 
     private int putMoveIntoBoard(int col, int row, int turn) {
-        if (turn == 1) {
+        if (turn == 1 && board[row][col] == '-') {
             turn++;
             board[col-1][row-1] = 'X';
         }
@@ -44,26 +45,26 @@ class TicTacToe {
 
     public boolean checkWin() {
         if ((board[0][0] == 'X' && board[0][1] == 'X' && board[0][2] == 'X') ||
-            (board[1][0] == 'X' && board[1][1] == 'X' && board[1][2] == 'X') ||
-            (board[2][0] == 'X' && board[2][1] == 'X' && board[2][2] == 'X') ||
-            (board[0][0] == 'X' && board[1][0] == 'X' && board[2][0] == 'X') ||
-            (board[0][1] == 'X' && board[1][1] == 'X' && board[2][1] == 'X') ||
-            (board[0][2] == 'X' && board[1][2] == 'X' && board[2][2] == 'X') ||
-            (board[0][0] == 'X' && board[1][1] == 'X' && board[2][2] == 'X') ||
-            (board[0][2] == 'X' && board[1][1] == 'X' && board[2][0] == 'X'))
+                (board[1][0] == 'X' && board[1][1] == 'X' && board[1][2] == 'X') ||
+                (board[2][0] == 'X' && board[2][1] == 'X' && board[2][2] == 'X') ||
+                (board[0][0] == 'X' && board[1][0] == 'X' && board[2][0] == 'X') ||
+                (board[0][1] == 'X' && board[1][1] == 'X' && board[2][1] == 'X') ||
+                (board[0][2] == 'X' && board[1][2] == 'X' && board[2][2] == 'X') ||
+                (board[0][0] == 'X' && board[1][1] == 'X' && board[2][2] == 'X') ||
+                (board[0][2] == 'X' && board[1][1] == 'X' && board[2][0] == 'X'))
         {
             printBoard();
             System.out.println("First player wins...");
             return false;
         }
         if ((board[0][0] == '0' && board[0][1] == '0' && board[0][2] == '0') ||
-            (board[1][0] == '0' && board[1][1] == '0' && board[1][2] == '0') ||
-            (board[2][0] == '0' && board[2][1] == '0' && board[2][2] == '0') ||
-            (board[0][0] == '0' && board[1][0] == '0' && board[2][0] == '0') ||
-            (board[0][1] == '0' && board[1][1] == '0' && board[2][1] == '0') ||
-            (board[0][2] == '0' && board[1][2] == '0' && board[2][2] == '0') ||
-            (board[0][0] == '0' && board[1][1] == '0' && board[2][2] == '0') ||
-            (board[0][2] == '0' && board[1][1] == '0' && board[2][0] == '0'))
+                (board[1][0] == '0' && board[1][1] == '0' && board[1][2] == '0') ||
+                (board[2][0] == '0' && board[2][1] == '0' && board[2][2] == '0') ||
+                (board[0][0] == '0' && board[1][0] == '0' && board[2][0] == '0') ||
+                (board[0][1] == '0' && board[1][1] == '0' && board[2][1] == '0') ||
+                (board[0][2] == '0' && board[1][2] == '0' && board[2][2] == '0') ||
+                (board[0][0] == '0' && board[1][1] == '0' && board[2][2] == '0') ||
+                (board[0][2] == '0' && board[1][1] == '0' && board[2][0] == '0'))
         {
             printBoard();
             System.out.println("Second player wins...");
